20109a705a3e1718194f4447e257be26c3ebfebd
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2024-08-05 20:16:04 -0400

    Adding literals

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -12,7 +12,7 @@ public class Scanner {
     private int start = 0;
     private int line = 1;
 
-    public Scanner(String source) {
+    public Scanner(final String source) {
         this.source = source;
     }
 
@@ -26,51 +26,115 @@ public class Scanner {
     }
 
     private void scanToken() {
-        char c = advance();
-        
+        final char c = advance();
+
         switch (c) {
-            case '(': addToken(TokenType.LEFT_PAREN); break;
-            case ')': addToken(TokenType.RIGHT_PAREN); break;
-            case '{': addToken(TokenType.LEFT_BRACE); break;
-            case '}': addToken(TokenType.RIGHT_BRACE); break;
-            case ',': addToken(TokenType.COMMA); break;
-            case '.': addToken(TokenType.DOT); break;
-            case '-': addToken(TokenType.MINUS); break;
-            case '+': addToken(TokenType.PLUS); break;
-            case ';': addToken(TokenType.SEMICOLON); break;
-            case '*': addToken(TokenType.STAR); break;
-            case '!': 
-                      addToken(matchAdvance('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-                      break;
-            case '=': 
-                      addToken(matchAdvance('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
-                      break;
-            case '<': 
-                      addToken(matchAdvance('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-                      break;
-            case '>': 
-                      addToken(matchAdvance('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
-                      break;
-            case '/': 
-                      if (matchAdvance('/')) {
-                          while (peek() != '\n' && !isEnd()) {
-                              advance();
-                          }
-                      } else {
-                          addToken(TokenType.SLASH);
-                      }
-                      break;
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case ';':
+                addToken(TokenType.SEMICOLON);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
+            case '!':
+                addToken(matchAdvance('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(matchAdvance('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(matchAdvance('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(matchAdvance('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '/':
+                if (matchAdvance('/')) {
+                    while (peek() != '\n' && !isEnd()) {
+                        advance();
+                    }
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
             case ' ':
             case '\t':
             case '\r':
-                      break;
+                break;
             case '\n':
-                      line++;
-                      break;
+                line++;
+                break;
+            case '"':
+                string();
             default:
-                      Lox.error(line, "Unexpected character.");
-                      break;
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+                break;
         }
+    }
+
+    private boolean isDigit(final char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        } else if (peek() == '.') {
+            Lox.error(line, "Unexpected character in number");
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while (peek() != '"' && !isEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        final String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
     }
 
     private char peek() {
@@ -82,7 +146,13 @@ public class Scanner {
 
     }
 
-    private boolean matchAdvance(char value) {
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean matchAdvance(final char value) {
         if (isEnd()) {
             return false;
         }
@@ -99,15 +169,14 @@ public class Scanner {
         return source.charAt(current++);
     }
 
-    private void addToken(TokenType type) {
+    private void addToken(final TokenType type) {
         addToken(type, null);
     }
 
-    private void addToken(TokenType type, Object literal) {
-        String text = source.substring(start, current);
+    private void addToken(final TokenType type, final Object literal) {
+        final String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
-
 
     private boolean isEnd() {
         return current >= source.length();
